c2e7221d240bb8f9ec7dd6d25a38688e48eb6725
Matthew Findley <findley@substantial.com>
2016-11-11 09:31:12 -0800

    Change meta to command

--- my/my-native-keybindings.el
+++ my/my-native-keybindings.el
@@ -2,4 +2,7 @@
 (define-key key-translation-map "\C-j" "\C-x")
 (global-set-key (kbd "M-j") nil)
 
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
 (provide 'my-native-keybindings)
