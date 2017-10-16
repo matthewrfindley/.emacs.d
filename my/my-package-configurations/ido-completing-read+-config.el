a91aeaca5b075d81678fb0bfebc599b9c91dbce6
Matt Findley <findley@substantial.com>
2017-10-16 11:25:40 -0700

    Add fuzzy finding via ido-completing-read+

---
+++ my/my-package-configurations/ido-completing-read+-config.el
@@ -0,0 +1,13 @@
+;;; ido-completing-read+-config --- Summary
+;;; Commentary:
+;;; Code:
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  (custom-set-variables
+   '(ido-mode 1)
+   '(ido-everywhere 1))
+  )
+
+(provide 'ido-completing-read+)
+;;; ido-completing-read+-config.el ends here
