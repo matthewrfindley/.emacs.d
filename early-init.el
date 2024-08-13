c587b00f127cf0344a3722ad50ecfb6b8aa840ed
Matt Findley <findley@substantial.com>
2024-08-12 18:44:02 -0700

    Latest

--- early-init.el
+++ early-init.el
@@ -9,17 +9,11 @@
         '(min-width . 1) '(width . 81)
         '(vertical-scroll-bars . nil)
         '(internal-border-width . 0)
-        '(left-fringe . 16)
-        '(right-fringe . 16)
         '(tool-bar-lines . 0)
         '(menu-bar-lines . 0)
-        '(undecorated-round . t)
         '(inhibit-double-buffering . t)
-        ;; ns-auto-titlebar will take care of this for any other frames and when
-        ;; switching to a dark theme, but this is set here to avoid loading
-        ;; ns-auto-titlebar on init
         '(ns-transparent-titlebar . t)
-        '(ns-appearance . light))
+        '(ns-appearance . dark))
        default-frame-alist))
 
 (setq ns-use-thin-smoothing t
