6c9ff291e0b1f7f543a6b07943cf1b65681749b0
Matt Findley <findley@substantial.com>
2026-01-20 18:29:08 -0800

    Dired will auto updated when visiting a new directory

--- my/my-package-configurations/dired-efap-config.el
+++ my/my-package-configurations/dired-efap-config.el
@@ -1,12 +1,15 @@
-;;; company-config --- Summary  -*- lexical-binding: t; -*-
+;;; dired-efap-config --- Summary  -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
+(use-package dired
+  :ensure nil
+  :config
+  (setq dired-auto-revert-buffer t))
+
 (use-package dired-efap
   :ensure t
   :config
-  (custom-set-variables
-   '(dired-efap-initial-filename-selection nil))
-  )
+  (setq dired-efap-initial-filename-selection nil))
 
 (provide 'dired-efap-config)
 ;;; dired-efap-config.el ends here
