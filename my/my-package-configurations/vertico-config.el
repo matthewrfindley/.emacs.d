4034bf867209a764afb805973b2cf10f1f66ed81
Matt Findley <findley@substantial.com>
2026-01-27 15:55:33 -0800

    Embark

--- my/my-package-configurations/vertico-config.el
+++ my/my-package-configurations/vertico-config.el
@@ -19,6 +19,33 @@
   :init
   (marginalia-mode))
 
+;; Embark - actions on completion candidates
+(use-package embark
+  :bind (:map vertico-map
+         ("C-c C-o" . embark-export)
+         ("C-c C-c" . embark-act)
+         ("C-c C-e" . my/embark-export-wgrep))
+  :preface
+  (defvar my/embark-export-wgrep-flag nil)
+  (defun my/embark-export-wgrep ()
+    "Export and enable wgrep in one step."
+    (interactive)
+    (setq my/embark-export-wgrep-flag t)
+    (command-execute #'embark-export)))
+
+(use-package embark-consult
+  :after (embark consult)
+  :hook (embark-collect-mode . consult-preview-at-point-mode)
+  :config
+  (declare-function wgrep-change-to-wgrep-mode "ext:wgrep")
+  (defun my/embark-export-grep-wgrep (&rest _)
+    "Start wgrep after export when flagged."
+    (when my/embark-export-wgrep-flag
+      (setq my/embark-export-wgrep-flag nil)
+      (when (eq major-mode 'grep-mode)
+        (wgrep-change-to-wgrep-mode))))
+  (advice-add #'embark-consult-export-grep :after #'my/embark-export-grep-wgrep))
+
 ;; Consult - enhanced commands
 (use-package consult
   :bind (("M-x" . execute-extended-command)
